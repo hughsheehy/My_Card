35d9110796c0c6404221d1f40fa5e1657590c6cb
hugh sheehy <hughsheehy@users.noreply.github.com>
2020-06-10 14:29:44 +0100

    Image, adapt to HSH
    Image, adapt to HSH

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,76 +6,130 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final String _text =
+      'www.o6ointernational.com https://www.o6ointernational.com';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: SafeArea(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/angela.jpg'),
+              radius: 50,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage('images/hugh.webp'),
             ),
             Text(
-              'Angela Yu',
+              'Hugh Sheehy',
               style: TextStyle(
                 fontFamily: 'Pacifico',
-                fontSize: 40.0,
                 color: Colors.white,
+                fontSize: 40,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Text(
-              'FLUTTER DEVELOPER',
+              'GROWTH, TECHNOLOGY, TRANSFORMATION',
               style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
+                  color: Colors.teal.shade50,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5),
             ),
             SizedBox(
-              height: 20.0,
-              width: 150.0,
+              height: 20,
+              width: 150,
               child: Divider(
-                color: Colors.teal.shade100,
+                color: Colors.blue.shade50,
               ),
             ),
             Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                title: Text(
+                  '+353 831411179',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
                   ),
-                  title: Text(
-                    '+44 123 456 789',
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                title: Text(
+                  'hugh.sheehy@insead.edu',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.alternate_email,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                title: Text(
+                  'hugh.sheehy@gmail.com',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.web,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                title: RichText(
+                  text: TextSpan(
+                    text: 'www.o6ointernational.com',
                     style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
+                      color: Colors.blue,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2,
                     ),
                   ),
-                )),
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    'angela@email.com',
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro'),
-                  ),
-                ))
+                ),
+              ),
+            ),
           ],
         )),
       ),
